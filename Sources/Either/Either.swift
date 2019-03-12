904fe0435ca100dc9a06983cebcef135478d0084
Stephen Celis <stephen@stephencelis.com>
2019-03-12 18:23:19 -0400

    Stuff

--- Sources/Either/Either.swift
+++ Sources/Either/Either.swift
@@ -1,226 +1,83 @@
-public enum Either<L, R> {
-  case left(L)
-  case right(R)
-}
+public enum Either<Left, Right> {
+  case left(Left)
+  case right(Right)
 
-extension Either {
-  public func either<A>(_ l2a: (L) throws -> A, _ r2a: (R) -> A) rethrows -> A {
+  public init(_ optional: Right?, default value: @autoclosure () -> Left) {
+    self = optional.map(Either.right) ?? Either.left(value())
+  }
+
+  public var left: Left? {
+    return self.either(ifLeft: Optional.some, ifRight: { _ in Optional.none })
+  }
+
+  public var right: Right? {
+    return self.either(ifLeft: { _ in Optional.none }, ifRight: Optional.some)
+  }
+
+  public func either<Value>(
+    ifLeft: (Left) throws -> Value,
+    ifRight: (Right) throws -> Value
+    ) rethrows -> Value {
     switch self {
-    case let .left(l):
-      return try l2a(l)
-    case let .right(r):
-      return r2a(r)
+    case let .left(left):
+      return try ifLeft(left)
+    case let .right(right):
+      return try ifRight(right)
     }
   }
 
-  public var left: L? {
-    return either(Optional.some, { _ in .none })
-  }
-
-  public var right: R? {
-    return either({ _ in .none }, Optional.some)
-  }
-
-  public var isLeft: Bool {
-    return either({ _ in true }, { _ in false })
-  }
-
-  public var isRight: Bool {
-    return either(const(false), const(true))
-  }
-}
-
-public func either<A, B, C>(_ a2c: @escaping (A) -> C, _ b2c: @escaping (B) -> C) -> (Either<A, B>) -> C {
-  return { ab in
-    ab.either(a2c, b2c)
-  }
-}
-
-public func lefts<S: Sequence, L, R>(_ xs: S) -> [L] where S.Element == Either<L, R> {
-  return xs |> mapOptional { $0.left }
-}
-
-public func rights<S: Sequence, L, R>(_ xs: S) -> [R] where S.Element == Either<L, R> {
-  return xs |> mapOptional { $0.right }
-}
-
-public func note<L, R>(_ default: L) -> (R?) -> Either<L, R> {
-  return optional(.left(`default`)) <| Either.right
-}
-
-public func hush<L, R>(_ lr: Either<L, R>) -> R? {
-  return lr.either(const(.none), R?.some)
-}
-
-public extension Either where L == Error {
-  public static func wrap<A>(_ f: @escaping (A) throws -> R) -> (A) -> Either {
-    return { a in
-      do {
-        return .right(try f(a))
-      } catch let error {
-        return .left(error)
-      }
-    }
-  }
-
-  public static func wrap(_ f: @escaping () throws -> R) -> Either {
-    do {
-      return .right(try f())
-    } catch let error {
-      return .left(error)
-    }
-  }
-
-  public func unwrap() throws -> R {
-    return try either({ throw $0 }, id)
-  }
-}
-
-public extension Either where L: Error {
-  public func unwrap() throws -> R {
-    return try either({ throw $0 }, id)
-  }
-}
-
-public func unwrap<R>(_ either: Either<Error, R>) throws -> R {
-  return try either.unwrap()
-}
-
-public func unwrap<L: Error, R>(_ either: Either<L, R>) throws -> R {
-  return try either.unwrap()
-}
-
-// MARK: - Functor
-
-extension Either {
-  public func map<A>(_ r2a: (R) -> A) -> Either<L, A> {
+  public func `do`(
+    ifLeft: (Left) throws -> Void,
+    ifRight: (Right) throws -> Void
+    ) rethrows {
     switch self {
-    case let .left(l):
-      return .left(l)
-    case let .right(r):
-      return .right(r2a(r))
+    case let .left(left):
+      try ifLeft(left)
+    case let .right(right):
+      try ifRight(right)
     }
   }
 
-  public static func <¢> <A>(r2a: (R) -> A, lr: Either) -> Either<L, A> {
-    return lr.map(r2a)
+  public func bimap<NewLeft, NewRight>(
+    ifLeft transformLeft: (Left) throws -> NewLeft,
+    ifRight transformRight: (Right) throws -> NewRight
+    ) rethrows -> Either<NewLeft, NewRight> {
+    return try self.either(
+      ifLeft: { .left(try transformLeft($0)) },
+      ifRight: { .right(try transformRight($0)) }
+    )
+  }
+
+  public func mapLeft<NewLeft>(
+    _ transform: (Left) throws -> NewLeft
+    ) rethrows -> Either<NewLeft, Right> {
+    return try self.bimap(ifLeft: transform, ifRight: { $0 })
+  }
+
+  public func mapRight<NewRight>(
+    _ transform: (Right) throws -> NewRight
+    ) rethrows -> Either<Left, NewRight> {
+    return try self.bimap(ifLeft: { $0 }, ifRight: transform)
+  }
+
+  public func flatMapLeft<NewLeft>(
+    _ transform: (Left) throws -> Either<NewLeft, Right>
+    ) rethrows -> Either<NewLeft, Right> {
+    return try self.either(ifLeft: transform, ifRight: { .right($0) })
+  }
+
+  public func flatMapRight<NewRight>(
+    _ transform: (Right) throws -> Either<Left, NewRight>
+    ) rethrows -> Either<Left, NewRight> {
+    return try self.either(ifLeft: { .left($0) }, ifRight: transform)
   }
 }
 
-public func map<A, B, C>(_ b2c: @escaping (B) -> C) -> (Either<A, B>) -> Either<A, C> {
-  return { ab in
-    b2c <¢> ab
-  }
+public struct Errors: Error {
+  let errors: [Error]
 }
 
-// MARK: - Bifunctor
-
-extension Either {
-  public func bimap<A, B>(_ l2a: (L) -> A, _ r2b: (R) -> B) -> Either<A, B> {
-    switch self {
-    case let .left(l):
-      return .left(l2a(l))
-    case let .right(r):
-      return .right(r2b(r))
-    }
-  }
-}
-
-public func bimap<A, B, C, D>(_ a2b: @escaping (A) -> B, _ c2d: @escaping (C) -> D)
-  -> (Either<A, C>)
-  -> Either<B, D> {
-    return { ac in
-      ac.bimap(a2b, c2d)
-    }
-}
-
-// MARK: - Apply
-
-extension Either {
-  public func apply<A>(_ r2a: Either<L, (R) -> A>) -> Either<L, A> {
-    return r2a.flatMap(self.map)
-  }
-
-  public static func <*> <A>(r2a: Either<L, (R) -> A>, lr: Either<L, R>) -> Either<L, A> {
-    return lr.apply(r2a)
-  }
-}
-
-public func apply<A, B, C>(_ b2c: Either<A, (B) -> C>) -> (Either<A, B>) -> Either<A, C> {
-  return { ab in
-    b2c <*> ab
-  }
-}
-
-// MARK: - Applicative
-
-public func pure<L, R>(_ r: R) -> Either<L, R> {
-  return .right(r)
-}
-
-// MARK: - Traversable
-
-public func traverse<S, E, A, B>(
-  _ f: @escaping (A) -> Either<E, B>
-  )
-  -> (S)
-  -> Either<E, [B]>
-  where S: Sequence, S.Element == A {
-
-    return { xs in
-      var ys: [B] = []
-      for x in xs {
-        let y = f(x)
-        switch y {
-        case let .left(e):
-          return .left(e)
-        case let .right(y):
-          ys.append(y)
-        }
-      }
-      return .right(ys)
-    }
-}
-
-/// Returns first `left` value in array of `Either`'s, or an array of `right` values if there are no `left`s.
-public func sequence<E, A>(_ xs: [Either<E, A>]) -> Either<E, [A]> {
-  return xs |> traverse(id)
-}
-
-// MARK: - Alt
-
-extension Either: Alt {
-  public static func <|> (lhs: Either, rhs: @autoclosure @escaping () -> Either) -> Either {
-    switch lhs {
-    case .left:
-      return rhs()
-    case .right:
-      return lhs
-    }
-  }
-}
-
-// MARK: - Bind/Monad
-
-extension Either {
-  public func flatMap<A>(_ r2a: (R) -> Either<L, A>) -> Either<L, A> {
-    return either(Either<L, A>.left, r2a)
-  }
-}
-
-public func flatMap <L, R, A>(_ r2a: @escaping (R) -> Either<L, A>) -> (Either<L, R>) -> Either<L, A> {
-  return { lr in
-    lr.flatMap(r2a)
-  }
-}
-
-public func >=> <E, A, B, C>(f: @escaping (A) -> Either<E, B>, g: @escaping (B) -> Either<E, C>) -> (A) -> Either<E, C> {
-  return f >>> flatMap(g)
-}
-
-// MARK: - Eq/Equatable
-
-extension Either: Equatable where L: Equatable, R: Equatable {
+extension Either: Equatable where Left: Equatable, Right: Equatable {
   public static func == (lhs: Either, rhs: Either) -> Bool {
     switch (lhs, rhs) {
     case let (.left(lhs), .left(rhs)):
@@ -233,9 +90,7 @@ extension Either: Equatable where L: Equatable, R: Equatable {
   }
 }
 
-// MARK: - Ord/Comparable
-
-extension Either: Comparable where L: Comparable, R: Comparable {
+extension Either: Comparable where Left: Comparable, Right: Comparable {
   public static func < (lhs: Either, rhs: Either) -> Bool {
     switch (lhs, rhs) {
     case let (.left(lhs), .left(rhs)):
@@ -250,71 +105,55 @@ extension Either: Comparable where L: Comparable, R: Comparable {
   }
 }
 
-// MARK: - Foldable/Sequence
-
-extension Either where R: Sequence {
-  public func reduce<A>(_ a: A, _ f: @escaping (A, R.Element) -> A) -> A {
-    return self.map(Prelude.reduce(f) <| a).either(const(a), id)
+extension Either: Hashable where Left: Hashable, Right: Hashable {
+  public func hash(into hasher: inout Hasher) {
+    self.do(
+      ifLeft: { hasher.combine($0) },
+      ifRight: { hasher.combine($0) }
+    )
   }
 }
 
-public func foldMap<S: Sequence, M: Monoid, L>(_ f: @escaping (S.Element) -> M) -> (Either<L, S>) -> M {
-  return { xs in
-    xs.reduce(.empty) { accum, x in accum <> f(x) }
-  }
-}
-
-// MARK: - Semigroup
-
-extension Either: Semigroup where R: Semigroup {
-  public static func <> (lhs: Either, rhs: Either) -> Either {
-    return curry(<>) <¢> lhs <*> rhs
-  }
-}
-
-// MARK: - NearSemiring
-
-extension Either: NearSemiring where R: NearSemiring {
-  public static func + (lhs: Either, rhs: Either) -> Either {
-    return curry(+) <¢> lhs <*> rhs
-  }
-
-  public static func * (lhs: Either, rhs: Either) -> Either {
-    return curry(*) <¢> lhs <*> rhs
-  }
-
-  public static var zero: Either {
-    return .right(R.zero)
-  }
-}
-
-// MARK: - Semiring
-
-extension Either: Semiring where R: Semiring {
-  public static var one: Either {
-    return .right(R.one)
-  }
-}
-
-// MARK: - Codable
-
-extension Either: Decodable where L: Decodable, R: Decodable {
+extension Either: Decodable where Left: Decodable, Right: Decodable {
   public init(from decoder: Decoder) throws {
     do {
-      self = try .right(.init(from: decoder))
-    } catch {
-      self = try .left(.init(from: decoder))
+      self = try .left(Left(from: decoder))
+    } catch let leftError {
+      do {
+        self = try .right(Right(from: decoder))
+      } catch let rightError {
+        throw DecodingError.typeMismatch(
+          Either.self,
+          .init(
+            codingPath: decoder.codingPath,
+            debugDescription: "Could not decode \(Left.self) or \(Right.self)",
+            underlyingError: Errors(errors: [leftError, rightError])
+          )
+        )
+      }
     }
   }
 }
 
-extension Either: Encodable where L: Encodable, R: Encodable {
+extension Either: Encodable where Left: Encodable, Right: Encodable {
   public func encode(to encoder: Encoder) throws {
-    switch self {
-    case let .left(l):
-      try l.encode(to: encoder)
-    case let .right(r):
-      try r.encode(to: encoder)
-    }
+    return try self.either(
+      ifLeft: { try $0.encode(to: encoder) },
+      ifRight: { try $0.encode(to: encoder) }
+    )
   }
 }
+
+#if swift(>=5.0)
+extension Either where Left: Error {
+  public var asRightResult: Result<Right, Left> {
+    return self.either(ifLeft: Result.failure, ifRight: Result.success)
+  }
+}
+
+extension Either where Right: Error {
+  public var asLeftResult: Result<Left, Right> {
+    return self.either(ifLeft: Result.success, ifRight: Result.failure)
+  }
+}
+#endif
--- Sources/Either/Nested.swift
+++ Sources/Either/Nested.swift
@@ -4,105 +4,158 @@ public typealias E4<A, B, C, Z> = Either<A, E3<B, C, Z>>
 public typealias E5<A, B, C, D, Z> = Either<A, E4<B, C, D, Z>>
 public typealias E6<A, B, C, D, E, Z> = Either<A, E5<B, C, D, E, Z>>
 public typealias E7<A, B, C, D, E, F, Z> = Either<A, E6<B, C, D, E, F, Z>>
+public typealias E8<A, B, C, D, E, F, G, Z> = Either<A, E7<B, C, D, E, F, G, Z>>
+public typealias E9<A, B, C, D, E, F, G, H, Z> = Either<A, E8<B, C, D, E, F, G, H, Z>>
+public typealias E10<A, B, C, D, E, F, G, H, I, Z> = Either<A, E9<B, C, D, E, F, G, H, I, Z>>
+public typealias E11<A, B, C, D, E, F, G, H, I, J, Z> = Either<A, E10<B, C, D, E, F, G, H, I, J, Z>>
 
-public typealias Either1<A> = E2<A, Never>
-public typealias Either2<A, B> = E3<A, B, Never>
-public typealias Either3<A, B, C> = E4<A, B, C, Never>
-public typealias Either4<A, B, C, D> = E5<A, B, C, D, Never>
-public typealias Either5<A, B, C, D, E> = E6<A, B, C, D, E, Never>
-public typealias Either6<A, B, C, D, E, F> = E7<A, B, C, D, E, F, Never>
+public enum Done {}
 
-public func inj1<A, Z>(_ v: A) -> E2<A, Z> {
-  return .left(v)
+extension Done: Decodable {
+  public init(from decoder: Decoder) throws {
+    switch self {}
+  }
 }
 
-public func inj2<A, B, Z>(_ v: B) -> E3<A, B, Z> {
-  return .right(.left(v))
+extension Done: Encodable {
+  public func encode(to encoder: Encoder) throws {
+  }
 }
 
-public func inj3<A, B, C, Z>(_ v: C) -> E4<A, B, C, Z> {
-  return .right(.right(.left(v)))
-}
+public typealias Either1<A> = E2<A, Done>
+public typealias Either2<A, B> = E3<A, B, Done>
+public typealias Either3<A, B, C> = E4<A, B, C, Done>
+public typealias Either4<A, B, C, D> = E5<A, B, C, D, Done>
+public typealias Either5<A, B, C, D, E> = E6<A, B, C, D, E, Done>
+public typealias Either6<A, B, C, D, E, F> = E7<A, B, C, D, E, F, Done>
+public typealias Either7<A, B, C, D, E, F, G> = E8<A, B, C, D, E, F, G, Done>
+public typealias Either8<A, B, C, D, E, F, G, H> = E9<A, B, C, D, E, F, G, H, Done>
+public typealias Either9<A, B, C, D, E, F, G, H, I> = E10<A, B, C, D, E, F, G, H, I, Done>
+public typealias Either10<A, B, C, D, E, F, G, H, I, J> = E11<A, B, C, D, E, F, G, H, I, J, Done>
 
-public func inj4<A, B, C, D, Z>(_ v: D) -> E5<A, B, C, D, Z> {
-  return .right(.right(.right(.left(v))))
-}
+extension Either {
+  public static func in1(_ value: Left) -> Either {
+    return .left(value)
+  }
 
-public func inj5<A, B, C, D, E, Z>(_ v: E) -> E6<A, B, C, D, E, Z> {
-  return .right(.right(.right(.right(.left(v)))))
-}
+  public static func in2<A, B>(_ value: A) -> Either
+    where Right == E2<A, B> {
+      return .right(.left(value))
+  }
 
-public func inj6<A, B, C, D, E, F, Z>(_ v: F) -> E7<A, B, C, D, E, F, Z> {
-  return .right(.right(.right(.right(.right(.left(v))))))
-}
+  public static func in3<A, B, C>(_ value: B) -> Either
+    where Right == E3<A, B, C> {
+      return .right(.right(.left(value)))
+  }
 
-public func get1<A, Z>(_ e: E2<A, Z>) -> A? {
-  if case let .left(v) = e { return v }
-  return nil
-}
+  public static func in4<A, B, C, D>(_ value: C) -> Either
+    where Right == E4<A, B, C, D> {
+      return .right(.right(.right(.left(value))))
+  }
 
-public func get2<A, B, Z>(_ e: E3<A, B, Z>) -> B? {
-  if case let .right(.left(v)) = e { return v }
-  return nil
-}
+  public static func in5<A, B, C, D, E>(_ value: D) -> Either
+    where Right == E5<A, B, C, D, E> {
+      return .right(.right(.right(.right(.left(value)))))
+  }
 
-public func get3<A, B, C, Z>(_ e: E4<A, B, C, Z>) -> C? {
-  if case let .right(.right(.left(v))) = e { return v }
-  return nil
-}
+  public static func in6<A, B, C, D, E, F>(_ value: E) -> Either
+    where Right == E6<A, B, C, D, E, F> {
+      return .right(.right(.right(.right(.right(.left(value))))))
+  }
 
-public func get4<A, B, C, D, Z>(_ e: E5<A, B, C, D, Z>) -> D? {
-  if case let .right(.right(.right(.left(v)))) = e { return v }
-  return nil
-}
+  public static func in7<A, B, C, D, E, F, G>(_ value: F) -> Either
+    where Right == E7<A, B, C, D, E, F, G> {
+      return .right(.right(.right(.right(.right(.right(.left(value)))))))
+  }
 
-public func get5<A, B, C, D, E, Z>(_ e: E6<A, B, C, D, E, Z>) -> E? {
-  if case let .right(.right(.right(.right(.left(v))))) = e { return v }
-  return nil
-}
+  public static func in8<A, B, C, D, E, F, G, H>(_ value: G) -> Either
+    where Right == E8<A, B, C, D, E, F, G, H> {
+      return .right(.right(.right(.right(.right(.right(.right(.left(value))))))))
+  }
 
-public func get6<A, B, C, D, E, F, Z>(_ e: E7<A, B, C, D, E, F, Z>) -> F? {
-  if case let .right(.right(.right(.right(.right(.left(v)))))) = e { return v }
-  return nil
-}
+  public static func in9<A, B, C, D, E, F, G, H, I>(_ value: H) -> Either
+    where Right == E9<A, B, C, D, E, F, G, H, I> {
+      return .right(.right(.right(.right(.right(.right(.right(.right(.left(value)))))))))
+  }
 
-public func either3<A, B, C, D>(
-  _ either: Either3<A, B, C>,
-  _ a2d: (A) -> D,
-  _ b2d: (B) -> D,
-  _ c2d: (C) -> D
-  )
-  -> D {
-    switch either {
-    case let .left(a):
-      return a2d(a)
-    case let .right(.left(b)):
-      return b2d(b)
-    case let .right(.right(.left(c))):
-      return c2d(c)
-    }
-}
+  public static func in10<A, B, C, D, E, F, G, H, I, J>(_ value: I) -> Either
+    where Right == E10<A, B, C, D, E, F, G, H, I, J> {
+      return .right(.right(.right(.right(.right(.right(.right(.right(.right(.left(value))))))))))
+  }
 
-public func at1<A, R, Z>(_ v: R, _ f: @escaping (A) -> R) -> (E2<A, Z>) -> R {
-  return { get1($0).map(f) ?? v }
-}
+  public func at1<R>(_ transform: (Left) -> R) -> R? {
+    return self.left.map(transform)
+  }
 
-public func at2<A, B, R, Z>(_ v: R, _ f: @escaping (B) -> R) -> (E3<A, B, Z>) -> R {
-  return { get2($0).map(f) ?? v }
-}
+  public func at2<A, B, R>(_ transform: (A) -> R) -> R?
+    where Right == E2<A, B> {
+      guard
+        case let .right(.left(value)) = self
+        else { return nil }
+      return transform(value)
+  }
 
-public func at3<A, B, C, R, Z>(_ v: R, _ f: @escaping (C) -> R) -> (E4<A, B, C, Z>) -> R {
-  return { get3($0).map(f) ?? v }
-}
+  public func at3<A, B, C, R>(_ transform: (B) -> R) -> R?
+    where Right == E3<A, B, C> {
+      guard
+        case let .right(.right(.left(value))) = self
+        else { return nil }
+      return transform(value)
+  }
 
-public func at4<A, B, C, D, R, Z>(_ v: R, _ f: @escaping (D) -> R) -> (E5<A, B, C, D, Z>) -> R {
-  return { get4($0).map(f) ?? v }
-}
+  public func at4<A, B, C, D, R>(_ transform: (C) -> R) -> R?
+    where Right == E4<A, B, C, D> {
+      guard
+        case let .right(.right(.right(.left(value)))) = self
+        else { return nil }
+      return transform(value)
+  }
 
-public func at5<A, B, C, D, E, R, Z>(_ v: R, _ f: @escaping (E) -> R) -> (E6<A, B, C, D, E, Z>) -> R {
-  return { get5($0).map(f) ?? v }
-}
+  public func at5<A, B, C, D, E, R>(_ transform: (D) -> R) -> R?
+    where Right == E5<A, B, C, D, E> {
+      guard
+        case let .right(.right(.right(.right(.left(value))))) = self
+        else { return nil }
+      return transform(value)
+  }
 
-public func at6<A, B, C, D, E, F, R, Z>(_ v: R, _ f: @escaping (F) -> R) -> (E7<A, B, C, D, E, F, Z>) -> R {
-  return { get6($0).map(f) ?? v }
+  public func at6<A, B, C, D, E, F, R>(_ transform: (E) -> R) -> R?
+    where Right == E6<A, B, C, D, E, F> {
+      guard
+        case let .right(.right(.right(.right(.right(.left(value)))))) = self
+        else { return nil }
+      return transform(value)
+  }
+
+  public func at7<A, B, C, D, E, F, R, G>(_ transform: (F) -> R) -> R?
+    where Right == E7<A, B, C, D, E, F, G> {
+      guard
+        case let .right(.right(.right(.right(.right(.right(.left(value))))))) = self
+        else { return nil }
+      return transform(value)
+  }
+
+  public func at8<A, B, C, D, E, F, G, H, R>(_ transform: (G) -> R) -> R?
+    where Right == E8<A, B, C, D, E, F, G, H> {
+      guard
+        case let .right(.right(.right(.right(.right(.right(.right(.left(value)))))))) = self
+        else { return nil }
+      return transform(value)
+  }
+
+  public func at9<A, B, C, D, E, F, G, H, I, R>(_ transform: (H) -> R) -> R?
+    where Right == E9<A, B, C, D, E, F, G, H, I> {
+      guard
+        case let .right(.right(.right(.right(.right(.right(.right(.right(.left(value))))))))) = self
+        else { return nil }
+      return transform(value)
+  }
+
+  public func at10<A, B, C, D, E, F, G, H, I, J, R>(_ transform: (I) -> R) -> R?
+    where Right == E10<A, B, C, D, E, F, G, H, I, J> {
+      guard
+        case let .right(.right(.right(.right(.right(.right(.right(.right(.right(.left(value)))))))))) = self
+        else { return nil }
+      return transform(value)
+  }
 }
